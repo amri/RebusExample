a8f503776e442266d0af3428b3b0a7c4c90bf603
Amri Hidayat <amri.hidayat@gmail.com>
2016-02-13 20:31:53 +0800

    Simple console test

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rebus.Activation;
+using Rebus.Config;
 
 namespace RebusExample
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                var connectionString = ConfigurationManager.AppSettings["serviceBus"];
+                Configure.With(activator)
+                    .Transport(t => t.UseAzureServiceBus(connectionString, "rebusQueue")).Start();
+            }
+            Console.WriteLine("q");
+            Console.ReadLine();
+
+
         }
     }
 }
