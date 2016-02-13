08477cf27ec639a8228fd1690434f9a58de1b8c2
Amri Hidayat <amri.hidayat@gmail.com>
2016-02-13 21:26:26 +0800

    update to show send local

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using Rebus.Activation;
 using Rebus.Config;
+using Rebus.Handlers;
 
 namespace RebusExample
 {
@@ -15,14 +17,33 @@ namespace RebusExample
         {
             using (var activator = new BuiltinHandlerActivator())
             {
+                activator.Register(() => new PrintDateTime());
+
                 var connectionString = ConfigurationManager.AppSettings["serviceBus"];
                 Configure.With(activator)
                     .Transport(t => t.UseAzureServiceBus(connectionString, "rebusQueue")).Start();
+                var timer = new Timer();
+                timer.Elapsed += delegate
+                {
+                    activator.Bus.SendLocal(DateTime.Now).Wait();
+
+                };
+                timer.Interval = 1000;
+                timer.Start();
+
+                Console.ReadLine();
             }
-            Console.WriteLine("q");
-            Console.ReadLine();
+            
 
 
+        }
+    }
+
+    internal class PrintDateTime : IHandleMessages<DateTime>
+    {
+        public async Task Handle(DateTime message)
+        {
+            Console.WriteLine("The time is {0}",message);
         }
     }
 }
